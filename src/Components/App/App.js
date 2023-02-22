c61f3f22116caccb6e9d932a6cc6822e98568c48
violetacf <violeta@cique.es>
2023-02-22 17:42:04 +0000

    added created by

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,7 @@ import Header from '../Header/Header';
 import About from '../About/About';
 import NavBar from '../NavBar/NavBar';
 import Gallery from '../Gallery/Gallery';
+import CreatedBy from '../Created/Created';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         </Routes>
         <SocialMediaLinks />
       </BrowserRouter>
+      <CreatedBy />
     </div>
   );
 }
---
+++ src/Components/Created/Created.js
@@ -0,0 +1,13 @@
+import './Created.css';
+
+export default function CreatedBy() {
+  return (
+    <div className="created-container">
+      <p>
+        Developed and designed by{' '}
+        <a href="https://violeta-portfolio.onrender.com/">Violeta Cique</a> -
+        2023
+      </p>
+    </div>
+  );
+}

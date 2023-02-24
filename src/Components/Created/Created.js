b2e36fc80060692bcef20d55a9a48cba803bcbb2
violetacf <violeta@cique.es>
2023-02-24 13:38:51 +0000

    little leaves added

--- src/Components/Created/Created.js
+++ src/Components/Created/Created.js
@@ -1,13 +1,15 @@
 import './Created.css';
+import Leaves from '../../Images/Leaves2.png';
 
 export default function CreatedBy() {
   return (
     <div className="created-container">
+      <img className="left-leaf leaves" src={Leaves} alt="leaves"></img>
       <p>
-        Designed & Developed By
-        <a href="https://violeta.cique.es/">Violeta Cique</a>-
-        2023
+        Designed & Developed by
+        <a href="https://violeta.cique.es/">Violeta Cique</a>- 2023
       </p>
+      <img className="right-leaf leaves" src={Leaves} alt="leaves"></img>
     </div>
   );
 }

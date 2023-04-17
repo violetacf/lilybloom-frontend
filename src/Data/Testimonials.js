8d1e5b7a5e2094a73e28fcd7fdfcde52ec17acac
violetacf <violeta@cique.es>
2023-04-17 17:46:50 +0100

    working on testimonials

---
+++ src/Data/Testimonials.js
@@ -0,0 +1,29 @@
+let testimonials = [
+  {
+    quote: 'Lil is great',
+    author: 'Pepe Flowers',
+    category: 'wedding flowers',
+  },
+  {
+    quote: 'Lil has great attention to detail',
+    author: 'Marta Sanchez',
+    category: 'fabric painting',
+  },
+  {
+    quote: 'Lil organised the vent very well',
+    author: 'Susan Sarandon',
+    category: 'wedding flowers',
+  },
+  {
+    quote: 'This person said something good too',
+    author: 'Another Person',
+    category: 'wedding flowers',
+  },
+  {
+    quote: 'Lil is great',
+    author: 'Pepe Flowers',
+    category: 'wedding flowers',
+  },
+];
+
+export default testimonials;
